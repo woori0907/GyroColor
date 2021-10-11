7d745b7c48a71c96c91af986ee4443463845002a
Woori <rose5307@naver.com>
2021-10-11 17:19:42 +0900

    modify gyroscope function : from raw value to degree

--- colorChange.js
+++ colorChange.js
@@ -2,11 +2,16 @@
 const viewAngleX = document.getElementById("viewAngleX");
 const viewAngleY = document.getElementById("viewAngleY");
 let gyroscope = new Gyroscope({frequncy : 60});
+let angleX = 0;
+let angleY = 0;
+let pi = Math.PI;
 
 // get and print current x and y axis
 const getGyroscope = () => {
-    viewAngleX.innerText = `X-axis : ${gyroscope.x}`;
-    viewAngleY.innerText = `Y-axis : ${gyroscope.y}`;
+    angleX = (angleX + gyroscope.x * 60) * 180 / pi;
+    angleY = (angleY + gyroscope.y * 60) * 180 / pi;
+    viewAngleX.innerText = `X-axis : ${angleX}`;
+    viewAngleY.innerText = `Y-axis : ${angleY}`;
 }
 
 gyroscope.addEventListener('reading', getGyroscope);
