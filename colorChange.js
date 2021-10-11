ab8e94bb5a7219562c9773b8cd2d15e0e59b0169
Woori <rose5307@naver.com>
2021-10-11 16:55:05 +0900

    make function that  get and print current x and y axis

---
+++ colorChange.js
@@ -0,0 +1,13 @@
+// 변수 불러오기
+const viewAngleX = document.getElementById(viewAngleX);
+const viewAngleY = document.getElementById(viewAngleY);
+let gyroscope = new Gyroscope({frequncy : 60});
+
+// get and print current x and y axis
+const getGyroscope = () => {
+    viewAngleX.innerText = `X-axis : ${gyroscope.x}`;
+    viewAngleY.innerText = `Y-axis : ${gyroscope.y}`;
+}
+
+gyroscope.addEventListener('reading', getGyroscope);
+gyroscope.start();
