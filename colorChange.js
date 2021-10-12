d6dbcaa0f31cb29680feea3fcb2b9af69b28293b
Woori <rose5307@naver.com>
2021-10-12 16:00:20 +0900

    make function that get random color

--- colorChange.js
+++ colorChange.js
@@ -2,20 +2,24 @@
 const viewAngleX = document.getElementById("viewAngleX");
 const viewAngleY = document.getElementById("viewAngleY");
 let gyroscope = new Gyroscope({frequncy : 60});
-let angleX = 0;
-let angleY = 0;
-let radianX = 0;
-let radianY = 0;
-let pi = Math.PI;
+let firstColor;
+let secondColor;
+
+const randomColor = () =>{
+    const index = Math.floor(Math.random() * colors.length);
+    console.log(index);
+    console.log(colors[index]);
+    return colors[index];
+}
 
 // get and print current x and y axis
 const getGyroscope = () => {
-
-    angleX = Math.floor((gyroscope.x * 180) / pi);
-    angleY = Math.floor((gyroscope.y * 180) / pi);
-    viewAngleX.innerText = `X-axis : ${angleX}`;
-    viewAngleY.innerText = `Y-axis : ${angleY}`;
+    firstColor = randomColor();
+    secondColor = randomColor();
+    document.body.style.backgroundImgae = `linear-gradient(to top, ${firstColor}, 0%, ${secondColor}, 100%)`
+    // background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
 }
+
 
 gyroscope.addEventListener('reading', getGyroscope);
 gyroscope.start();
---
+++ colorData.js
@@ -0,0 +1,10 @@
+const colors = [
+    '#ff9a9e',
+    '#fad0c4',
+    '#a18cd1',
+    '#fbc2eb',
+    '#fad0c4',
+    '#ffd1ff',
+    '#ffecd2',
+    '#fcb69f',
+]
