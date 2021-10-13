165dde578fbc4f37be27416dc88ef8c69e744cc0
Woori <rose5307@naver.com>
2021-10-13 17:02:27 +0900

    add gradient background fade and change function

--- colorChange.js
+++ colorChange.js
@@ -1,25 +1,44 @@
 // 변수 불러오기
 const viewAngleX = document.getElementById("viewAngleX");
 const viewAngleY = document.getElementById("viewAngleY");
-let gyroscope = new Gyroscope({frequency : 0.5});
-let firstColor;
-let secondColor;
+let gradientMask1 = document.getElementById("gradientMask_1");
+let gradientMask2 = document.getElementById("gradientMask_2");
 
 const randomColor = () =>{
     const index = Math.floor(Math.random() * colors.length);
     return colors[index];
 }
 
-// get and print current x and y axis
-const getGyroscope = () => {
+const initColor = () => {
     firstColor = randomColor();
     secondColor = randomColor();
-    console.log(firstColor);
-    console.log(secondColor);
-    document.body.style.backgroundImage = `linear-gradient(45deg, ${firstColor} 0%, ${secondColor} 100%)`;
-    // background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
+    if(firstColor===secondColor){
+        secondColor = randomColor();
+    }
+    gradientMask2.style.backgroundImage = `linear-gradient(45deg, ${firstColor} 0%, ${secondColor} 100%)`;
+    gradientMask2.style.opacity = 1;
+    gradientMask1.style.opacity = 0;
 }
 
+// get and print current x and y axis
+const changeColor = () => {
+    firstColor = randomColor();
+    secondColor = randomColor();
+    if(firstColor===secondColor){
+        secondColor = randomColor();
+    }
+    console.log(gradientMask1.style.opacity==0);
+    if(gradientMask1.style.opacity == 0){
+        gradientMask1.style.backgroundImage = `linear-gradient(45deg, ${firstColor} 0%, ${secondColor} 100%)`;
+        gradientMask1.style.opacity = 1;
+        gradientMask2.style.opacity = 0;
+    }
+    else{
+        gradientMask2.style.backgroundImage = `linear-gradient(45deg, ${firstColor} 0%, ${secondColor} 100%)`;
+        gradientMask2.style.opacity = 1;
+        gradientMask1.style.opacity = 0;
+    }
+}
 
-gyroscope.addEventListener('reading', getGyroscope);
-gyroscope.start();
+initColor();
+setInterval(changeColor, 3000);
