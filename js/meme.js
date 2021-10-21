52a96c3f1ff1d9341060837b60bd814347b19b0f
Woori <rose5307@naver.com>
2021-10-21 13:30:10 +0900

    load memes function complete

--- js/meme.js
+++ js/meme.js
@@ -1,11 +1,13 @@
 const btnGetMeme = document.getElementById("btnGetMeme");
 const memeTitle = document.getElementById("memeTitle");
 const memeDesc = document.getElementById("memeDesc");
+const memeImg = document.getElementById("memeImg");
 
 
 // 랜덤하게 밈 데이터를 받아오는 함수
 const printMeme = () => {
     const randomNum = Math.floor(Math.random()*memes.length);
+    memeImg.src = memes[randomNum].image;
     memeTitle.innerText = memes[randomNum].text;
     memeDesc.innerText = memes[randomNum].description;
 }
--- js/memeData.js
+++ js/memeData.js
@@ -1,56 +1,56 @@
 const memes = [
     {
-        image: '',
+        image: 'img/memes/stop.png',
         text: "멈춰!",
         description: "그런다고 멈추겠냐"
     },
     {
-        image: '',
+        image: 'img/memes/muyaho.jpg',
         text: "무야호~",
         description: '그만큼 신나시는 거지'
     },
     {
-        image: '',
+        image: 'img/memes/hug.jpg',
         text: "그냥 꼭안아주면되..",
         description: '맞춤법 틀림'
     },
     {
-        image: '',
+        image: 'img/memes/choijun.png',
         text: "철이 없었죠",
         description: '커피가 좋아서 유학을 했다는 게'
     },
     {
-        image: '',
+        image: 'img/memes/wtf.jpg',
         text: "어? 너 뭐하는?",
         description: '얘 한 마흔 돼 보이는데?'
     },
     {
-        image: '',
+        image: 'img/memes/possible.jpg',
         text: "가능한",
         description: '김종민 씨. 한 번 해봐요.'
     },
     {
-        image: '',
+        image: 'img/memes/gentle.jpg',
         text: "제법 젠틀해요",
         description: '젠젠젠 젠틀맨이다'
     },
     {
-        image: '',
+        image: 'img/memes/shuck.jpg',
         text: "슈슉 슈숙. 슉. 시. 시발럼아.",
         description: '이건 입에서 나오는 소리가 아니여'
     },
     {
-        image: '',
+        image: 'img/memes/funcoolsexy.jpg',
         text: "그것이 약속이니까(뿌듯)",
         description: '환쿠루섹쿠시'
     },
     {
-        image: '',
+        image: 'img/memes/beanmouse.jpg',
         text: "콩쥐야 좆대써...",
         description: '콩쥐야 나는 이제 틀렸어..'
     },
     {
-        image: '',
+        image: 'img/memes/gganbu.png',
         text: "우린 깐부잖아",
         description: '깐부끼리는 네 거 내 거가 없는 거야.'
     },
