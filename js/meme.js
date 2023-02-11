7a90eceefa0a379049bdfb693859fc2fbb687536
Woori <rose5307@naver.com>
2023-02-11 22:55:41 +0900

    음식 데이터 불러오기, 랜덤 데이터 출력 기능 구현

--- js/meme.js
+++ js/meme.js
@@ -3,13 +3,24 @@ const memeTitle = document.getElementById("memeTitle");
 const memeDesc = document.getElementById("memeDesc");
 const memeImg = document.getElementById("memeImg");
 
-
 // 랜덤하게 밈 데이터를 받아오는 함수
-const printMeme = () => {
-    const randomNum = Math.floor(Math.random()*memes.length);
-    memeImg.src = memes[randomNum].image;
-    memeTitle.innerText = memes[randomNum].text;
-    memeDesc.innerText = memes[randomNum].description;
-}
+let recipeData;
+const getRecipes = async () => {
+  const data = await (
+    await fetch(
+      `http://openapi.foodsafetykorea.go.kr/api/eafaaf00c0a245adb5ea/COOKRCP01/json/1/1000`
+    )
+  ).json();
+  recipeData = data.COOKRCP01.row;
+  console.log("Data Loaded!");
+};
 
-btnGetMeme.addEventListener("click", printMeme);
+const printRecipe = () => {
+  const index = Math.floor(Math.random() * 1001);
+  console.log(recipeData[index]);
+};
+
+window.onload = () => {
+  getRecipes();
+};
+btnGetMeme.addEventListener("click", printRecipe);
